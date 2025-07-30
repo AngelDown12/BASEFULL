b29be711ec1c0bcb20c4ad806d4da9611335c7fb
AngelDown12 <angeldown12xz@gmail.com>
2025-07-30 01:26:51 -0600

    Actualizar sticker-sticker.js

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -2,19 +2,22 @@ import { sticker } from '../lib/sticker.js'
 import uploadFile from '../lib/uploadFile.js'
 import uploadImage from '../lib/uploadImage.js'
 import { webp2png } from '../lib/webp2mp4.js'
+
 let handler = async (m, { conn, args, usedPrefix, command }) => {
   let stiker = false
   let username = conn.getName(m.sender)
+
   try {
-  	
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || q.mediaType || ''
+    
     if (/webp|image|video/g.test(mime)) {
       let img = await q.download?.()
       if (!img) return m.reply(`☁️ Responde a una *Imagen* o *Vídeo.*`)
       let out
+
       try {
-        stiker = await sticker(img, false, global.packname, global.author)
+        stiker = await sticker(img, false, '', '') // sin texto abajo
       } catch (e) {
         console.error(e)
       } finally {
@@ -23,12 +26,13 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
           else if (/image/g.test(mime)) out = await uploadImage(img)
           else if (/video/g.test(mime)) out = await uploadFile(img)
           if (typeof out !== 'string') out = await uploadImage(img)
-          stiker = await sticker(false, out, global.packname, global.author)
+
+          stiker = await sticker(false, out, '', '') // también sin texto abajo
         }
       }
     } else if (args[0]) {
-      if (isUrl(args[0])) stiker = await sticker(false, args[0], global.packname, global.author)
-      else return m.reply('La *Url* es invalida')
+      if (isUrl(args[0])) stiker = await sticker(false, args[0], '', '') // sin texto también
+      else return m.reply('La *Url* es inválida.')
     }
   } catch (e) {
     console.error(e)
@@ -38,9 +42,10 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     else return m.reply('☁️ Responde a una *Imagen* o *Vídeo.*')
   }
 }
+
 handler.help = ['sticker']
 handler.tags = ['sticker']
-handler.command = ['s', 'sticker', 'stiker'] 
+handler.command = ['s', 'sticker', 'stiker']
 
 export default handler
 
