bc4a3bc127c58a472b93ab021f7804cb6eae5cee
AngelDown12 <angeldown12xz@gmail.com>
2025-07-30 01:57:09 -0600

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -1,79 +1,14 @@
-import axios from 'axios';
-import fs from 'fs';
-import path from 'path';
-import sharp from 'sharp';
-import {
-    tmpdir
-} from 'os';
-const delay = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+import { sticker } from '../lib/sticker.js'
 
-const fetchSticker = async (text, attempt = 1) => {
-    try {
-        const response = await axios.get(`https://kepolu-brat.hf.space/brat`, {
-            params: {
-                q: text
-            },
-            responseType: 'arraybuffer',
-        });
-        return response.data;
-    } catch (error) {
-        if (error.response?.status === 429 && attempt <= 3) {
-            const retryAfter = error.response.headers['retry-after'] || 5;
-            await delay(retryAfter * 1000);
-            return fetchSticker(text, attempt + 1);
-        }
-        throw error;
-    }
-};
-
-const handler = async (m, {
-    text,
-    conn
-}) => {
-    if (!text) {
-        return conn.sendMessage(m.chat, {
-            text: `${emoji} Por favor ingresa el texto para hacer un sticker.`,
-        }, {
-            quoted: m
-        });
-    }
-
-    try {
-        const buffer = await fetchSticker(text);
-        const outputFilePath = path.join(tmpdir(), `sticker-${Date.now()}.webp`);
-        await sharp(buffer)
-            .resize(512, 512, {
-                fit: 'contain',
-                background: {
-                    r: 255,
-                    g: 255,
-                    b: 255,
-                    alpha: 0
-                }
-            })
-            .webp({
-                quality: 80
-            })
-            .toFile(outputFilePath);
-
-        await conn.sendMessage(m.chat, {
-            sticker: {
-                url: outputFilePath
-            },
-        }, {
-            quoted: fkontak
-        });
-        fs.unlinkSync(outputFilePath);
-    } catch (error) {
-        return conn.sendMessage(m.chat, {
-            text: `${msm} Ocurrio un error.`,
-        }, {
-            quoted: m
-        });
-    }
-};
-handler.command = ['brat'];
-handler.tags = ['sticker'];
-handler.help = ['brat *<texto>*'];
-
-export default handler;
+let handler = async (m, { conn, text, command }) => {
+let user = global.db.data.users[m.sender]
+let f = user.packname || global.packname
+let g = (user.packname && user.author ? user.author : (user.packname && !user.author ? '' : global.author))
+let stiker = await sticker(null, global.API('caliphdev', `/api/brat`, { text: text }), f, g)
+if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
+throw stiker.toString()
+}
+handler.help = ['brat <texto>']
+handler.tags = ['sticker']
+handler.command = /^brat$/i
+export default handler
