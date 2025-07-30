795204ecc8712402af9d10ea6baa561f9f067548
AngelDown12 <angeldown12xz@gmail.com>
2025-07-30 01:59:31 -0600

    Actualizar sticker-brat.js

--- plugins/sticker-brat.js
+++ plugins/sticker-brat.js
@@ -1,14 +1,9 @@
-import { sticker } from '../lib/sticker.js'
+const handler = async (m, { conn, args }) => 
+  args[0] && conn.sendMessage(m.chat, {
+    sticker: { url: `https://api.siputzx.my.id/api/m/brat?text=${encodeURIComponent(args.join(' '))}` },
+    packname: '', author: ''
+  });
 
-let handler = async (m, { conn, text, command }) => {
-let user = global.db.data.users[m.sender]
-let f = user.packname || global.packname
-let g = (user.packname && user.author ? user.author : (user.packname && !user.author ? '' : global.author))
-let stiker = await sticker(null, global.API('caliphdev', `/api/brat`, { text: text }), f, g)
-if (stiker) return conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
-throw stiker.toString()
-}
-handler.help = ['brat <texto>']
-handler.tags = ['sticker']
-handler.command = /^brat$/i
-export default handler
+handler.command = /^brat$/i;
+
+export default handler;
