11406a70e4d71efe81513cc30696ed9ee22edef7
AngelDown12 <angeldown12xz@gmail.com>
2025-07-30 02:16:46 -0600

    Actualizar group-link.js

--- plugins/group-link.js
+++ plugins/group-link.js
@@ -1,9 +1,6 @@
 const handler = async (m, { conn }) =>
   conn.sendMessage(m.chat, { text: 'https://chat.whatsapp.com/' + await conn.groupInviteCode(m.chat) });
 
-
-handler.customPrefix = /^(link)/i;
-handler.command = new RegExp;
 handler.command = ['link', 'enlace'];
 handler.group = true;
 handler.botAdmin = true;
